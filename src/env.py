2537ea7b919ef18323ba36442d99ebe8a052c8f2
DragonflyGuest <aguest.dragonflythinking@gmail.com>
2024-09-18 17:36:03 +1000

    Delete src directory

--- src/env.py
+++
@@ -1,9 +0,0 @@
-pedictor_path = "../model/shape_predictor_5_face_landmarks.dat"
-face_rec_model_path = "../model/dlib_face_recognition_resnet_model_v1.dat"
-
-
-faces_folder_base = "../entities/entities_raw"
-output_folder_base = "../entities/entities_process"
-
-raw_json_output = "../raw_json"
-raw_metadata = "../raw_data"
